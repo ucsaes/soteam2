331d99c65f1c6d5c8c8a3a2ccf1ae402a46ee88e
김민준 <kimminjun0215@gmail.com>
2025-01-18 14:02:03 +0900

    position done

--- src/debugging/cordinate.jsx
+++ src/debugging/cordinate.jsx
@@ -3,13 +3,13 @@ import './cordinate.css';
 
 function Cordinate() {
   const [position, setPosition] = useState({ x: 50, y: 50 });
-  const [rotation, setRotation] = useState(0); // 회전 상태 추가
+  const [rotation, setRotation] = useState(localStorage.getItem('r_value')); // 회전 상태 추가
   const [isMoving, setIsMoving] = useState(true);
 
   // 클릭한 위치로 이동하는 함수
   const handleClick = (e) => {
-    const x = (e.clientX / window.innerWidth) * 100; // 클릭한 위치의 비율
-    const y = (e.clientY / window.innerHeight) * 100;
+    const x = Number(((e.clientX / window.innerWidth) * 100).toFixed(1)); // 클릭한 위치의 비율
+    const y = Number(((e.clientY / window.innerHeight) * 100).toFixed(1));
     setPosition({ x, y });
   };
 
@@ -18,38 +18,43 @@ function Cordinate() {
     if (isMoving) {
       let newX = position.x;
       let newY = position.y;
+      let newR = rotation;
 
       // 방향키에 따른 위치 이동
       switch (e.key) {
         case 'ArrowUp':
-          newY = Math.max(0, position.y - 0.1); // 위로 이동
+          newY = Math.max(0, position.y - 0.05); // 위로 이동
           break;
         case 'ArrowDown':
-          newY = Math.min(100, position.y + 0.1); // 아래로 이동
+          newY = Math.min(100, position.y + 0.05); // 아래로 이동
           break;
         case 'ArrowLeft':
-          newX = Math.max(0, position.x - 0.1); // 왼쪽으로 이동
+          newX = Math.max(0, position.x - 0.05); // 왼쪽으로 이동
           break;
         case 'ArrowRight':
-          newX = Math.min(100, position.x + 0.1); // 오른쪽으로 이동
+          newX = Math.min(100, position.x + 0.05); // 오른쪽으로 이동
           break;
         case '2': // 1을 눌렀을 때 시계방향 회전
-          setRotation((prevRotation) => (prevRotation + 10) % 360);
+          newR = (newR + 10) % 360;
           break;
         case '1': // 2를 눌렀을 때 반시계방향 회전
-          setRotation((prevRotation) => (prevRotation - 10 + 360) % 360);
+          newR = (newR - 10 + 360) % 360;
           break;
         case 'w': // 1을 눌렀을 때 시계방향 회전
-          setRotation((prevRotation) => (prevRotation + 1) % 360);
+          newR = (newR + 1) % 360;
           break;
         case 'q': // 2를 눌렀을 때 반시계방향 회전
-          setRotation((prevRotation) => (prevRotation - 1 + 360) % 360);
+          newR = (newR - 1 + 360) % 360;
           break;
         default:
           return;
       }
-
       setPosition({ x: newX, y: newY });
+      setRotation(newR);
+      navigator.clipboard.writeText(
+        `[${newX.toFixed(2)}, ${(newY - 0.1).toFixed(2)}, ${newR}]`
+      );
+      localStorage.setItem('r_value', newR);
     }
   };
 
@@ -87,9 +92,9 @@ function Cordinate() {
             transform: `translate(-50%, -50%) rotate(${rotation}deg)`, // 중앙으로 위치 설정
           }}
         ></div>
-        <div className="cor">{`x: ${position.x.toFixed(
-          1
-        )}%, y: ${position.y.toFixed(1)}% rotate: ${rotation}deg`}</div>
+        <div className="cor">{`${position.x.toFixed(2)}, ${(
+          position.y - 0.1
+        ).toFixed(2)}, ${rotation}`}</div>
       </div>
     </div>
   );
