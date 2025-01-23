de6c553bf2351d103cc69f70051797314bacec38
sungjoonim <junjlim8513@gmail.com>
2025-01-24 00:26:35 +0900

    chore: remove unnecessary import in forge.config.js

--- electron/forge.config.js
+++ electron/forge.config.js
@@ -1,6 +1,3 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
-
 module.exports = {
   packagerConfig: {
     platform: "win32",
